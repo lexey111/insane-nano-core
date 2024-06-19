df7296abd40745ce120cb0dd33a7227fd8c7a373
Oleksii Koshkin <lexey111@gmail.com>
2024-06-19 20:27:08 +0200

    Initial commit

---
+++ js/dashboard-LAPDWA3D.js
@@ -0,0 +1,2 @@
+(()=>{console.log("Dashboard init");})();
+//# sourceMappingURL=dashboard-LAPDWA3D.js.map
---
+++ js/main-HH7VTLJE.js
@@ -0,0 +1,2 @@
+(()=>{console.log("Main init");})();
+//# sourceMappingURL=main-HH7VTLJE.js.map
