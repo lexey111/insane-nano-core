28a3fc114a3bfc04afc2fbaf70d25f12e2cbbe43
Oleksii Koshkin <lexey111@gmail.com>
2024-06-19 20:49:47 +0200

    flat structure

--- js/dashboard-LAPDWA3D.js
+++
@@ -1,2 +0,0 @@
-(()=>{console.log("Dashboard init");})();
-//# sourceMappingURL=dashboard-LAPDWA3D.js.map
--- js/main-HH7VTLJE.js
+++
@@ -1,2 +0,0 @@
-(()=>{console.log("Main init");})();
-//# sourceMappingURL=main-HH7VTLJE.js.map
